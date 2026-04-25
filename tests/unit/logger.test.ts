915f3a36558af19e07adb382310929927eced872
root <juanrm2112@gmail.com>
2026-04-25 18:16:46 +0000

    add fixes to test file

--- tests/unit/logger.test.ts
+++ tests/unit/logger.test.ts
@@ -28,29 +28,29 @@ describe('logger redaction (audit M9)', () => {
   });
 
   beforeEach(() => {
-    written = '';
+  written = '';
 
-    const stream = new PassThrough();
-    stream.on('data', (d) => (written += d.toString()));
+  const stream = new PassThrough();
+  stream.on('data', (d) => (written += d.toString()));
 
-    logger = pino(
-      {
-        level: 'debug',
-        redact: {
-          paths: [
-            'DATABASE_URL',
-            'err.config',
-            'err.request',
-            'err.response.headers'
-          ],
-          censor: '[REDACTED]'
-        },
-        serializers: {
-          err: pino.stdSerializers.err
-        }
+  logger = pino(
+    {
+      level: 'debug',
+      redact: {
+        paths: [
+          'DATABASE_URL',
+          'err.config',
+          'err.request',
+          'err.response.headers'
+        ],
+        censor: '[REDACTED]'
       },
-      stream
-    });
+      serializers: {
+        err: pino.stdSerializers.err
+      }
+    },
+    stream
+  });
 
 
     const flush = () => new Promise((r) => setImmediate(r));
